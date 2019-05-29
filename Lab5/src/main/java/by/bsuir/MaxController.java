873ddfce38d92b21de52d7e82f55dc342296b8d8
eugeen3 <vvgg42@gmail.com>
2019-05-29 19:12:40 +0300

    lab5 complete

--- Lab5/src/main/java/by/bsuir/MaxController.java
+++ Lab5/src/main/java/by/bsuir/MaxController.java
@@ -6,14 +6,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @Controller
 public class MaxController {
 	
-	@RequestMapping(value="/Max", method=RequestMethod.POST)
+	@RequestMapping(value="/Lab5_maxnumber", method=RequestMethod.POST)
 	@ResponseBody
-    public int compare(@RequestBody int[] values) {
-		FindMax comparator = new FindMax();
-		int result = comparator.compare(values);
-		return result;
+    public int compare(@RequestBody List<Integer> values) {
+	
+		return values.stream()
+				.max(Comparator.comparing(Integer::valueOf))
+                .get();
+		/*
+		int maxValue = values[0];
+		for (int i = 1; i < values.length; i++) {
+			if (values[i] > maxValue) 
+				maxValue = values[i];
+		}	
+        return maxValue;
     }
+    */
+	}
 }
