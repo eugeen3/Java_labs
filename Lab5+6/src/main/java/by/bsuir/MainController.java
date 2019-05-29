5ed774f56f92230509f59fe9c2d70daa6821439d
eugeen3 <vvgg42@gmail.com>
2019-05-29 20:27:15 +0300

    lab5+6 complete

---
+++ Lab5+6/src/main/java/by/bsuir/MainController.java
@@ -0,0 +1,61 @@
+package by.bsuir;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping(value="/Lab5_maxNumber", method=RequestMethod.POST)
+	@ResponseBody
+    public int maxInt(@RequestBody List<Integer> values) {
+		return values.stream()
+				.max(Comparator.comparing(Integer::intValue))
+                .get();
+	}
+	
+	@RequestMapping(value="/Lab6_addAndSubract", method=RequestMethod.POST)
+	@ResponseBody
+	 public List<Integer> reducingByElement(@RequestBody List<Integer> firstList, List<Integer> secondList) {
+		List<Integer> result = new ArrayList<Integer>();
+		
+		result.add(firstList.stream()
+					.mapToInt(Integer::intValue)
+					.sum()); 
+		
+		result.add(secondList.stream()
+				  		.reduce(0, (a, b) -> a - b));
+		
+		return result;
+	}
+	
+	@RequestMapping(value="/Lab6_strToInt", method=RequestMethod.POST)
+	@ResponseBody
+	 public List<Integer> stringToInt(@RequestBody List<String> list) {
+		return list.stream()
+				.map(s -> Integer.parseInt(s))
+				.collect(Collectors.toList());
+							
+	}
+	
+	@RequestMapping(value="/Lab6_maxLength", method=RequestMethod.POST)
+	@ResponseBody
+	 public String maxString(@RequestBody List<String> list) {
+		return list.stream()
+				.max(Comparator.comparing(String::length))
+                .get();
+							
+	}
+}
